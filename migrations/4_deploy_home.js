c9fa382d21365e197281eff3f57bbdf1a43528ed
Pepo <pepo.ospina@gmail.com>
2020-05-14 17:02:55 +0200

    remove deply home migration

--- migrations/4_deploy_home.js
+++
@@ -1,6 +0,0 @@
-const UprtclHomePerspectives = artifacts.require("UprtclHomePerspectives");
-
-module.exports = async (deployer, network, accounts) => {
-  const god = accounts[0];
-  deployer.deploy(UprtclHomePerspectives, { from: god });
-};
